25ec02d717a7099b9a98da7c10c49f092da7ff98
Alexis Guerin <aguerin42@icloud.com>
2017-05-05 22:56:26 +0200

    Les éléments sont listés et triés par ordre ASCII

--- src/ft_ls.c
+++ src/ft_ls.c
@@ -12,15 +12,57 @@
 
 #include "ft_ls.h"
 
+#include <sys/stat.h>
+#include <time.h>
+void	print(t_list *list)
+{
+	ft_putendl(list->content);
+}
+
+void	find_elem(char **argv, int size, t_ls ls)
+{
+	int			i;
+	struct stat	s;
+	t_list		*direc;
+	t_list		*files;
+	t_list		*nonex;
+if (ls.flags[0])
+	;
+	i = -1;
+	direc = NULL;
+	files = NULL;
+	nonex = NULL;
+	while (++i < size)
+	{
+		s.st_ino = 0;
+		lstat(argv[i], &s);
+		if (!s.st_ino)
+			nonex = ft_lstaddalpha(&nonex, ft_lstnew(argv[i], ft_strlen(argv[i])));
+		else if (S_ISDIR(s.st_mode))
+			direc = ft_lstaddalpha(&direc, ft_lstnew(argv[i], ft_strlen(argv[i])));
+		else
+			files = ft_lstaddalpha(&files, ft_lstnew(argv[i], ft_strlen(argv[i])));
+	}
+	ft_lstiter(nonex, &print);
+	ft_putendl("");
+	ft_lstiter(files, &print);
+	ft_putendl("");
+	ft_lstiter(direc, &print);
+}
+
 int	main(int argc, char **argv)
 {
 	int		i;
 	t_ls	ls;
 
 	i = 1;
-	fill_tab(ls.tab, NB_FLAG, 0);
+	fill_tab(ls.flags, NB_FLAG, 0);
 	if (argc > 1)
-		i += find_flags(&argv[i], ls);
-	ft_putendl(argv[i]);
+	{
+		i += find_flags(&argv[i], ls.flags);
+		find_elem(&argv[i], argc - i, ls);
+	}
+	/*else
+		print_directory(".", NULL);*/
 	return (0);
 }
