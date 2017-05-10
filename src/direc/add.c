b29df53d4c7c5fc6b46d9a68f256767117194832
Alexis Guerin <aguerin42@icloud.com>
2017-05-10 22:57:53 +0200

    Fonctionnel pour les flags -art mais print_directory à modifier

--- src/direc/add.c
+++ src/direc/add.c
@@ -4,9 +4,9 @@
 t_elem	*add_direc(t_elem *direc, char *name, t_ls *ls, struct stat s)
 {
 	if (ls->flags[4])
-		direc = ls_lstaddtime(&direc, ls_lstnew(name, "", s));
+		direc = ls_lstaddtime(&direc, ls_lstnew(name, name, s));
 	else
-		direc = ls_lstaddalpha(&direc, ls_lstnew(name, "", s));
+		direc = ls_lstaddalpha(&direc, ls_lstnew(name, name, s));
 	ls->nb_direc = 1;
 	return (direc);
 }
--- src/print/print_directory.c
+++ src/print/print_directory.c
@@ -11,20 +11,20 @@ void	print_directory(char *path, t_ls *ls)
 	struct stat		stat;
 	t_elem			*elem;
 
+	char *str = ft_strjoin(path, "/");
 	if ((direc = opendir(path)))
 	{
-		ft_printf("%s:\n", path);
+		if (ls->nb_direc > 1)
+			ft_printf("%s:\n", path);
 		elem = NULL;
 		while((content = readdir(direc)))
 		{
 			stat.st_ino = 0;
-			lstat(content->d_name, &stat);
+			lstat(ft_strjoin(str, content->d_name), &stat);
 			if (content->d_name[0] != '.' || ls->flags[1])
-			elem = add_files(elem, content->d_name, ls, stat);
+				elem = add_files(elem, content->d_name, ls, stat);
 		}
 		ls_files(&elem, ls);
-		if (ls)
-			;
 		closedir(direc);
 	}
 }
