17a9bc136efbfe56a00a6e17f05702009a5b7171
Alexis Guerin <aguerin@student.42.fr>
2017-05-25 15:12:09 +0200

    Correction des messages d'erreurs lorsque des fichiers existants et inexistants étaient donnés

--- src/nonex/ls_nonex.c
+++ src/nonex/ls_nonex.c
@@ -6,17 +6,39 @@
 /*   By: aguerin <aguerin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/11 15:54:17 by aguerin           #+#    #+#             */
-/*   Updated: 2017/05/18 12:43:48 by aguerin          ###   ########.fr       */
+/*   Updated: 2017/05/25 15:10:10 by aguerin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
+#include <stdlib.h>
 
-void	ls_nonex(t_list **nonex, t_ls *ls)
+static void	del(void *content, size_t size)
+{
+	if (size)
+		;
+	if (content)
+		free(content);
+	content = NULL;
+}
+
+static void	print_nonex(t_list *list)
+{
+	while (list)
+	{
+		ft_putstr_fd(NAME, 2);
+		ft_putstr_fd(": ", 2);
+		ft_putstr_fd((char*)list->content, 2);
+		ft_putendl_fd(": No such file or directory", 2);
+		list = list->next;
+	}
+}
+
+void	ls_nonex(t_list **nonex)
 {
 	if (*nonex)
 	{
-		print_nonex(*nonex, ls);
+		print_nonex(*nonex);
 		ft_lstdel(nonex, &del);
 	}
 }
