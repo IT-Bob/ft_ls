9f0e1bce49554fe9b0a0ee310c9acdf28b6adc4f
Alexis Guerin <aguerin@student.42.fr>
2017-05-25 17:49:14 +0200

    Sauv

--- src/flags/find.c
+++ src/flags/find.c
@@ -6,11 +6,12 @@
 /*   By: aguerin <aguerin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/05 17:11:53 by aguerin           #+#    #+#             */
-/*   Updated: 2017/05/05 17:38:21 by aguerin          ###   ########.fr       */
+/*   Updated: 2017/05/25 09:51:35 by aguerin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
+#include <stdlib.h>
 
 static int	is_valid_flag(char *flags, char c)
 {
@@ -26,16 +27,16 @@ static int	is_valid_flag(char *flags, char c)
 	return (-1);
 }
 
-static void	fill_flags_tab(char *flags, int tab[])
+static void	fill_flags_tab(char *argv, int flags[])
 {
 	int	i;
 	int	j;
 
 	j = -1;
-	while (flags[++j] && ((i = is_valid_flag(FLAGS, flags[j])) >= 0))
-		tab[i] = 1;
+	while (argv[++j] && ((i = is_valid_flag(FLAGS, argv[j])) >= 0))
+		flags[i] = 1;
 	if (i == -1)
-		illegal_option(flags[j]);
+		illegal_option(argv[j]);
 }
 
 int			find_flags(char **argv, int flags[])
@@ -43,7 +44,9 @@ int			find_flags(char **argv, int flags[])
 	int	i;
 
 	i = -1;
-	while (argv[++i] && argv[i][0] == '-')
+	if (!argv || !flags)
+		exit(-1);
+	while (argv && argv[++i] && argv[i][0] == '-')
 	{
 		if (!argv[i][1])
 			return (i);
