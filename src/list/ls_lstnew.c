2a29d98a22bb3f6d82eaef848eb7f9ad6d26e75d
Alexis Guerin <aguerin@student.42.fr>
2017-05-26 13:52:40 +0200

    Correction de leaks

--- src/list/ls_lstnew.c
+++ src/list/ls_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: aguerin <aguerin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/05 13:23:40 by aguerin           #+#    #+#             */
-/*   Updated: 2017/05/26 11:40:54 by aguerin          ###   ########.fr       */
+/*   Updated: 2017/05/26 13:49:42 by aguerin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,14 +23,49 @@
 
 
 // À PROTEGER
+void long_flag(t_elem *elem, struct stat stat)
+{
+	char	*date;
+	struct passwd	*pwd;
+	struct group	*grp;
+
+	date = NULL;
+	elem->link = stat.st_nlink;
+	elem->size = stat.st_size;
+	if (!(pwd = getpwuid(stat.st_uid)))
+		perror(NAME);
+	else
+	{
+		elem->user = ft_strnew(ft_strlen(pwd->pw_name));
+		elem->user = ft_strcpy(elem->user, pwd->pw_name);
+	}
+	if (!(grp = getgrgid(stat.st_gid)))
+		perror(NAME);
+	else
+	{
+		elem->grp = ft_strnew(ft_strlen(grp->gr_name));
+		elem->grp = ft_strcpy(elem->grp, grp->gr_name);
+	}
+	elem->date = ft_strnew(12);
+	if (time(NULL) - stat.st_mtime > 2628000 * 6)
+	{
+		date = ft_strsub(ctime(&elem->stat.st_mtime), 4, 7);
+		elem->date = ft_strcat(elem->date, date);
+		ft_strdel(&date);
+		date = ft_strsub(ctime(&elem->stat.st_mtime), 19, 5);
+	}
+	else
+		if (!(date = ft_strsub(ctime(&elem->stat.st_mtime), 4, 12)))
+			perror(NAME);
+	elem->date = ft_strcat(elem->date, date);
+	ft_strdel(&date);
+	elem->major = major(stat.st_rdev);
+	elem->minor = minor(stat.st_rdev);
+}
 
 t_elem	*ls_lstnew(char *name, char *path, struct stat stat, t_ls *ls)
 {
 	t_elem	*elem;
-	struct passwd	*pwd;
-	struct group	*grp;
-	char			*date;
-	char			*date2;
 
 	if ((elem = (t_elem*)ft_memalloc(sizeof(t_elem))))
 	{
@@ -45,39 +80,7 @@ t_elem	*ls_lstnew(char *name, char *path, struct stat stat, t_ls *ls)
 		elem->grp = NULL;
 		elem->date = NULL;
 		if (ls->flags[2])
-		{
-			date = NULL;
-			date2 = NULL;
-			elem->link = stat.st_nlink;
-			elem->size = stat.st_size;
-			if (!(pwd = getpwuid(stat.st_uid)))
-				perror(NAME);
-			else
-			{
-				elem->user = ft_strnew(ft_strlen(pwd->pw_name));
-				elem->user = ft_strcpy(elem->user, pwd->pw_name);
-			}
-			if (!(grp = getgrgid(stat.st_gid)))
-				perror(NAME);
-			else
-			{
-				elem->grp = ft_strnew(ft_strlen(grp->gr_name));
-				elem->grp = ft_strcpy(elem->grp, grp->gr_name);
-			}
-			if (time(NULL) - stat.st_mtime > 2628000 * 6)
-			{
-				date = ft_strsub(ctime(&elem->stat.st_mtime), 4, 7);
-				date2 = ft_strsub(ctime(&elem->stat.st_mtime), 19, 5);
-				elem->date = ft_strjoin(date, date2);									//leaks;
-			}
-			else
-				if (!(elem->date = ft_strsub(ctime(&elem->stat.st_mtime), 4, 12))) //leaks
-					perror(NAME);
-			ft_strdel(&date);
-			ft_strdel(&date2);
-			elem->major = major(stat.st_rdev);
-			elem->minor = minor(stat.st_rdev);
-		}
+			long_flag(elem, stat);
 		else
 		{
 			elem->user_len = 0;
