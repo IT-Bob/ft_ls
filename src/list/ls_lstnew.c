fb0ad2e8229411ec236b102e59ec1548a4a94c1f
Alexis Guerin <aguerin@student.42.fr>
2017-05-26 15:36:05 +0200

    Correction de norme

--- src/list/ls_lstnew.c
+++ src/list/ls_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: aguerin <aguerin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/05 13:23:40 by aguerin           #+#    #+#             */
-/*   Updated: 2017/05/26 13:49:42 by aguerin          ###   ########.fr       */
+/*   Updated: 2017/05/26 15:22:05 by aguerin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,32 @@
 #include <uuid/uuid.h>
 #include <sys/types.h>
 
-/*
-** ls_lstnew() alloue la mémoire et initialise la structure pour la liste de ls.
-*/
-
-
 // À PROTEGER
-void long_flag(t_elem *elem, struct stat stat)
+
+static void	date(t_elem *elem, struct stat stat)
 {
 	char	*date;
+
+	date = NULL;
+	elem->date = ft_strnew(12);
+	if (time(NULL) - stat.st_mtime > 2628000 * 6)
+	{
+		date = ft_strsub(ctime(&elem->stat.st_mtime), 4, 7);
+		elem->date = ft_strcat(elem->date, date);
+		ft_strdel(&date);
+		date = ft_strsub(ctime(&elem->stat.st_mtime), 19, 5);
+	}
+	else if (!(date = ft_strsub(ctime(&elem->stat.st_mtime), 4, 12)))
+		perror(NAME);
+	elem->date = ft_strcat(elem->date, date);
+	ft_strdel(&date);
+}
+
+static void	long_flag(t_elem *elem, struct stat stat)
+{
 	struct passwd	*pwd;
 	struct group	*grp;
 
-	date = NULL;
 	elem->link = stat.st_nlink;
 	elem->size = stat.st_size;
 	if (!(pwd = getpwuid(stat.st_uid)))
@@ -46,24 +59,22 @@ void long_flag(t_elem *elem, struct stat stat)
 		elem->grp = ft_strnew(ft_strlen(grp->gr_name));
 		elem->grp = ft_strcpy(elem->grp, grp->gr_name);
 	}
-	elem->date = ft_strnew(12);
-	if (time(NULL) - stat.st_mtime > 2628000 * 6)
-	{
-		date = ft_strsub(ctime(&elem->stat.st_mtime), 4, 7);
-		elem->date = ft_strcat(elem->date, date);
-		ft_strdel(&date);
-		date = ft_strsub(ctime(&elem->stat.st_mtime), 19, 5);
-	}
-	else
-		if (!(date = ft_strsub(ctime(&elem->stat.st_mtime), 4, 12)))
-			perror(NAME);
-	elem->date = ft_strcat(elem->date, date);
-	ft_strdel(&date);
+	date(elem, stat);
 	elem->major = major(stat.st_rdev);
 	elem->minor = minor(stat.st_rdev);
 }
 
-t_elem	*ls_lstnew(char *name, char *path, struct stat stat, t_ls *ls)
+static void	init(t_elem *elem)
+{
+	elem->user_len = 0;
+	elem->grp_len = 0;
+	elem->size_len = 0;
+	elem->link_len = 0;
+	elem->major = 0;
+	elem->minor = 0;
+}
+
+t_elem		*ls_lstnew(char *name, char *path, struct stat stat, t_ls *ls)
 {
 	t_elem	*elem;
 
@@ -82,14 +93,7 @@ t_elem	*ls_lstnew(char *name, char *path, struct stat stat, t_ls *ls)
 		if (ls->flags[2])
 			long_flag(elem, stat);
 		else
-		{
-			elem->user_len = 0;
-			elem->grp_len = 0;
-			elem->size_len = 0;
-			elem->link_len = 0;
-			elem->major = 0;
-			elem->minor = 0;
-		}
+			init(elem);
 		elem->next = NULL;
 	}
 	return (elem);
